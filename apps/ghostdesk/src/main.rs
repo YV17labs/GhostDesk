d48f162eed0adf54b8ba42a663bafe26859df0eb
Yoann Vanitou <yvanitou@gmail.com>
2026-08-17 11:18:34 -0400

    fix(app): Environment::init on line one of main

--- apps/ghostdesk/src/main.rs
+++ apps/ghostdesk/src/main.rs
@@ -1,8 +1,10 @@
 use ghostdesk::GhostdeskModule;
+use nest_rs::config::Environment;
 use nest_rs::core::App;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _environment = Environment::init();
     App::builder()
         .module::<GhostdeskModule>()
         .build()
